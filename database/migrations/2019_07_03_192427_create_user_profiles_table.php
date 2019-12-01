28494c65125dde1fbbea8598786a64a722aa31d4
kamilloo <kamil.pietka85@gmail.com>
2019-12-01 21:04:02 +0100

    user profile migration

--- database/migrations/2019_07_03_192427_create_user_profiles_table.php
+++ database/migrations/2019_07_03_192427_create_user_profiles_table.php
@@ -15,13 +15,15 @@ class CreateUserProfilesTable extends Migration
     {
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->unsignedInteger('user_id')->unique();
-            $table->string('address')->nullable()->default(null);
-            $table->string('company_name')->nullable()->default(null);
             $table->string('first_name')->nullable()->default(null);
             $table->string('last_name')->nullable()->default(null);
-            $table->string('services')->nullable()->default(null);
-            $table->string('logo')->nullable()->default(null);
+            $table->string('company_name')->nullable()->default(null);
+            $table->string('address')->nullable()->default(null);
+            $table->string('city')->nullable()->default(null);
+            $table->string('postcode')->nullable()->default(null);
+            $table->string('postcode')->nullable()->default(null);
             $table->string('avatar')->nullable()->default(null);
+            $table->string('services')->nullable()->default(null);
             $table->string('branch')->nullable()->default(null);
             $table->string('description')->nullable()->default(null);
             $table->timestamps();
