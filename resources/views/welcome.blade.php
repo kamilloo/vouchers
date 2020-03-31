363175dd05f2c06bc9f72854d2d5c5b241a6cfd8
kamilloo <kamil.pietka85@gmail.com>
2020-03-31 09:36:47 +0200

    change background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -372,7 +372,7 @@
                 <div class="logo">
                     <img src="welcome/img/logo.png" alt="">
                 </div>
-                <div class="copy-right-text">Copyright &copy; {{ \Carbon\Carbon::now()->year }}  |  All rights reserved to Dinomuz inc. This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></div>
+                <div class="copy-right-text">Copyright &copy; {{ \Carbon\Carbon::now()->year }}  |  All rights reserved to MyVouchers</div>
                 <div class="footer-social">
 {{--                    <a href="#"><i class="fa fa-facebook"></i></a>--}}
 {{--                    <a href="#"><i class="fa fa-twitter"></i></a>--}}
